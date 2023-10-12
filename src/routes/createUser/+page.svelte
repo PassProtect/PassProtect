1c9a8e79c3b60e1609cdb21ffe32d8cc67f64e51
Geoff Sun <geoffsun2@gmail.com>
2023-10-12 11:55:47 -0400

    updated dashboard

--- src/routes/createUser/+page.svelte
+++ src/routes/createUser/+page.svelte
@@ -4,12 +4,13 @@
 
 <div class="flex items-center justify-center h-screen">
 	<div id='container' class='h-1/2 w-1/2 bg-blue-500 flex items-center justify-center flex-col'>
-		<h1 class="">Log In Page</h1>
+		<h1 class="">Create User Page</h1>
+		<input placeholder="email;">
 		<input placeholder="username">
 		<input placeholder="password" type="password">
 		<div>
-			<button class='bg-gray-400 h-8 w-20'>Log In</button>
-			<button class='bg-gray-400 h-8 w-20'>Create Account</button>
+			<a href='/' class='bg-gray-400 h-8 w-20'>Create Account</a>
+			<a href='/' class='bg-gray-400 h-8 w-20'>Back to Log In</a>
 		</div>
 	</div>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,12 @@
 <script>
+  import { Card } from 'flowbite-svelte'
+
+  //make a call to db to find all accounts
+  //map through the array and make a card with the company name
+  const company = 'Netflix'
 </script>
 
-<div class="flex items-center justify-center h-screen">
-	<a href='/netflix'>Netflix</a>
-</div>
-
-
+<Card href="/cards">
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{company}</h5>
+  <p class="font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+</Card>
