8b8249bc44a817c7056503b42a329dd289eca48f
Geoff Sun <geoffsun2@gmail.com>
2023-10-17 13:06:13 -0400

    in progress decryption

--- src/routes/dashboard/[slug]/+page.svelte
+++ src/routes/dashboard/[slug]/+page.svelte
@@ -1,17 +1,19 @@
-<script>
-  import accountStore from '../../../Store.js';
+<script lang="ts">
+	import accountStore from '../../../Store.js';
 
-  $: companyname = $accountStore.companyname
-  $: url = $accountStore.url
-  $: username = $accountStore.username
-  $: password = $accountStore.password
+	$: companyname = $accountStore.companyname;
+	$: url = $accountStore.url;
+	$: username = $accountStore.username;
+
+  const plainPass = 'helo'
+  export let data;
 </script>
 
 <div class="w-screen h-screen flex flex-col justify-center items-center overflow-hidden">
-  <div class='border-2 border-blue-500 rounded-xl p-4'>
-    <h1 class='text-4xl my-4 text-center'>{companyname}</h1>
-    <h1 class='text-4xl my-4 text-center'>{url}</h1>
-    <h1 class='text-4xl my-4 text-center'>{username}</h1>
-    <h1 class='text-4xl my-4 text-center'>{password}</h1>
-  </div>
+	<div class="border-2 border-blue-500 rounded-xl p-4">
+		<h1 class="text-4xl my-4 text-center">{companyname}</h1>
+		<h1 class="text-4xl my-4 text-center">{url}</h1>
+		<h1 class="text-4xl my-4 text-center">{username}</h1>
+		<h1 class="text-4xl my-4 text-center">{plainPass}</h1>
+	</div>
 </div>
--- src/routes/dashboard/createAccount/+page.server.ts
+++ src/routes/dashboard/createAccount/+page.server.ts
@@ -1,22 +1,64 @@
-import type { Actions } from './types'
+import type { Actions } from '../../$types';
 import { pool } from '../../db';
+import crypto from 'crypto';
 
+function encrypt(text: string) {
+	const algorithm = 'aes-256-cbc';
+	const pass = process.env.VITE_KEY;
+	const key = crypto.scryptSync(pass, 'GfG', 32);
+	const iv = crypto.randomBytes(16);
+	const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+	let encrypted = cipher.update(text);
+
+	encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+	return { iv: iv.toString('hex'), data: encrypted.toString('hex') };
+}
+
+function decrypt(iv:string, encrypt:string) {
+	const pass = process.env.VITE_KEY;
+	const algorithm = 'aes-256-cbc';
+	const key = crypto.scryptSync(pass, 'GfG', 32);
+	const ivBuffer = Buffer.from(iv, 'hex');
+	const encryptedText = Buffer.from(encrypt, 'hex');
+	const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), ivBuffer);
+	let decrypted = decipher.update(encryptedText);
+	decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+	return decrypted.toString();
+}
 
 export const actions = {
-  default: async ({ request }) => {
-    const data = await request.formData();
-    const companyName = String(data.get('companyname'));
-    const url = data.get('url');
-    const username = data.get('username');
-    const password = data.get('password');
-    const user_id = 1;
- 
-    const queryString = 'INSERT INTO accounts (companyName, url, username, password, user_id) VALUES ($1, $2, $3, $4, $5);';
-    const queryValues = [companyName, url, username, password, user_id];
-    await pool.query(queryString, queryValues);
-    return {
-      status: 303,
-    }
-  },
-} satisfies Actions;
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const companyName = String(data.get('companyname'));
+		const url = String(data.get('url'));
+		const username = String(data.get('username'));
+		const password = String(data.get('password'));
+		const user_id = 1;
 
+		// const hashedPass = await Bun.password.hash(password, {
+		// 	algorithm: "bcrypt",
+		// 	cost: 4
+		// });
+		// console.log(hashedPass);
+		// const isMatch = await Bun.password.verify(password, hashedPass);
+		// console.log('MATCHES', isMatch);
+
+		// const key = crypto.randomBytes(32);
+
+		const encryptedPass = encrypt(password);
+		console.log(encryptedPass);
+		const decryptedPass = decrypt(encryptedPass.iv, encryptedPass.data);
+		console.log(decryptedPass);
+
+		const queryString =
+			'INSERT INTO accounts (companyName, url, username, password, iv, user_id) VALUES ($1, $2, $3, $4, $5, $6);';
+		const queryValues = [companyName, url, username, encryptedPass.data, encryptedPass.iv, user_id];
+		await pool.query(queryString, queryValues);
+		return {
+			status: 303
+		};
+	}
+} satisfies Actions;
