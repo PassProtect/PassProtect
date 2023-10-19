7f7d4dfa50ff924f99339179a4bdb8ee7ce2f557
Geoff Sun <geoffsun2@gmail.com>
2023-10-19 17:42:38 -0400

    cleaning comments

--- src/routes/(protected)/dashboard/+layout.svelte
+++ src/routes/(protected)/dashboard/+layout.svelte
@@ -1,74 +1,48 @@
 <script lang="ts">
-	import '../../../app.css'
+	import '../../../app.css';
 	import { onNavigate } from '$app/navigation';
 
+	onNavigate(async (navigation) => {
+		if (!document.startViewTransition) return;
 
-onNavigate(async (navigation) => {
-	if (!document.startViewTransition) return;
-
-	return new Promise((resolve) => {
-		document.startViewTransition(async () => {
-			resolve();
-			await navigation.complete;
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
 		});
 	});
-});
-
-// const clearCookie = async() => {
-// 	//actually get user_id
-// 	const user_id = 28
-// 	const info = {
-// 		user_id
-// 	}
-
-// 	const response = await fetch('/api/session', {
-// 		method: 'DELETE',
-// 		headers: {
-// 				'Content-Type': 'application/json'
-// 			},
-// 			body: JSON.stringify(info)
-// 	})
-
-// }
-
 </script>
 
-<!-- <nav class="bg-black/40 grow text-right sticky top-0 px-5 py-4 flex justify-between">
-	<div class="text-2xl align-middle">PassProtect</div>
-	<div>
-		<a href="/dashboard" class="px-2 align-middle">Dashboard</a>
-		<a href="/" class="px-2 align-middle"> Log Out </a>
-	</div>
-</nav> -->
 <main>
 	<slot />
 </main>
+
 <!-- <footer>
 	This is the footer
 </footer> -->
 <style>
 	@keyframes fade-in {
-	from {
-		opacity: 0;
+		from {
+			opacity: 0;
+		}
 	}
-}
 
-@keyframes fade-out {
-	to {
-		opacity: 0;
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
 	}
-}
 
-@keyframes slide-from-right {
-	from {
-		transform: translateX(30px);
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
 	}
-}
 
-@keyframes slide-to-left {
-	to {
-		transform: translateX(-30px);
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
 	}
-}
-
 </style>
